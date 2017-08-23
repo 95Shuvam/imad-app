31d59b9d4f193c205843ce28704f55fc07760f97
95Shuvam <sammycool.das63@gmail.com>
2017-08-24 03:57:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,20 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('article-one', function (req, res) {
-  res.send('article one responses will be send here');
+app.get('/article-one', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('article-two', function (req, res) {
-  res.send('article two responses will be send here');
+
+app.get('/article-two', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-app.get('article-three', function (req, res) {
-  res.send('article three responses will be send here');
+
+
+app.get('/article-three', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
